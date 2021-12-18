080f9840fe0f3e0128ba8372046ed87e9a21161e
cristian ortiz navia <cristian.ortiznavia82@gmail.com>
2021-12-17 23:57:53 -0300

    Update routes.go

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -6,13 +6,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-//Setup function defines groups por every module in app "api" "admin"
+//Setup function defines groups por every module in app like "api" or "admin"
 func Setup(app *fiber.App) {
 	//url prefix for api module routes
 	api := app.Group("api")
 	//url prefix for admin module inside api prefix
 	admin := api.Group("admin")
-	//this complete route is /api/admin/register
+	//this endpoint complete route is /api/admin/register
 	admin.Post("/register", controllers.Register)
 
 }
